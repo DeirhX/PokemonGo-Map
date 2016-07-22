a03c82dc860d1b793167c2a5643b5498ebb90266
deirh <deirh@seznam.cz>
2016-07-22 13:18:14 +0200

    Support for account list offset

--- generate_locations.py
+++ generate_locations.py
@@ -9,6 +9,7 @@ center_lon = 14.443
 center_lat = 50.078
 lon_size = 0.025
 lat_size = 0.025
+login_offset = 0
 
 base_lat = center_lat - lat_size * (float(cells_lat-1) / 2)
 base_lon = center_lon - lon_size* (float(cells_lon-1) / 2)
@@ -16,10 +17,13 @@ base_lon = center_lon - lon_size* (float(cells_lon-1) / 2)
 entries = []
 
 with open('accounts.txt') as acc_file:
+    while login_offset:
+        acc_file.readline()
+        login_offset -= 1
     for x in range(cells_lon):
         for y in range(cells_lat):
             username, password = acc_file.readline().split()
-            location = '{0} {1}'.format(base_lon + x * lon_size, base_lat + y * lat_size)
+            location = '{0} {1}'.format(base_lat + y * lat_size, base_lon + x * lon_size)
             entries.append({'username' : username, 'password' : password, 'location' : location})
 
 with open('locations.json', 'w') as file:
