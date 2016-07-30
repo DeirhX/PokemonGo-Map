138c66181ac4d6f451305027674e475bc66a7644
deirh <deirh@seznam.cz>
2016-07-30 03:16:06 +0200

    Adjusted so WSGI gets configured the same way as standalone

--- pogom/app.py
+++ pogom/app.py
@@ -14,7 +14,7 @@ from pogom.utils import get_args
 from . import config
 from .models import Pokemon, Gym, Pokestop, ScannedLocation
 from .search import search
-from ..runserver import configure
+from .startup import configure
 
 log = logging.getLogger(__name__)
 compress = Compress()
@@ -23,7 +23,7 @@ users = {}
 
 class Pogom(Flask):
     def __init__(self, import_name, **kwargs):
-        configure(self)
+        configure(self, args)
         super(Pogom, self).__init__(import_name, **kwargs)
         compress.init_app(self)
         self.json_encoder = CustomJSONEncoder
@@ -35,11 +35,7 @@ class Pogom(Flask):
         self.route("/next_loc", methods=['POST'])(self.next_loc)
         self.route("/mobile", methods=['GET'])(self.list_pokemon)
 
-        config['LOCALE'] = args.locale
-        config['CHINA'] = args.china
         config['ROOT_PATH'] = self.root_path
-        config['GMAPS_KEY'] = args.gmaps_key
-        config['REQ_SLEEP'] = args.scan_delay
 
     def fullmap(self):
         #args = get_args()
--- runserver.py
+++ runserver.py
@@ -17,16 +17,17 @@ from pogom.search import search_loop, create_search_threads, fake_search_loop
 from pogom.models import init_database, create_tables, drop_tables, Pokemon, Pokestop, Gym
 
 from pogom.pgoapi.utilities import get_pos_by_name
+from pogom.startup import configure
 
 logging.basicConfig(format='%(asctime)s [%(module)14s] [%(levelname)7s] %(message)s')
 log = logging.getLogger()
 app = Pogom(__name__)
-configured = False
 
 if __name__ == '__main__':
     args = get_args()
 
-    configure(app)
+    configure(app, args)
+    config['ROOT_PATH'] = app.root_path
 
     if not args.only_server:
         # Gather the pokemons!
@@ -45,8 +46,6 @@ if __name__ == '__main__':
     if args.cors:
         CORS(app);
 
-
-
     if args.no_server:
         # This loop allows for ctrl-c interupts to work since flask won't be holding the program open
         while search_thread.is_alive():
@@ -60,70 +59,3 @@ if __name__ == '__main__':
             app.run(threaded=True, use_reloader=False, debug=args.debug, host=args.host, port=args.port)
 
 
-def configure(app):
-
-    if configured:
-        return
-    configured = True
-
-    if args.debug:
-        log.setLevel(logging.DEBUG);
-    else:
-        log.setLevel(logging.INFO);
-
-
-    # Let's not forget to run Grunt / Only needed when running with webserver
-    if not args.no_server:
-        if not os.path.exists(os.path.join(os.path.dirname(__file__), 'static/dist')):
-            log.critical('Please run "grunt build" before starting the server.');
-            sys.exit();
-
-    # These are very noisey, let's shush them up a bit
-    logging.getLogger("peewee").setLevel(logging.INFO)
-    logging.getLogger("requests").setLevel(logging.WARNING)
-    logging.getLogger("pogom.pgoapi.pgoapi").setLevel(logging.WARNING)
-    logging.getLogger("pogom.pgoapi.rpc_api").setLevel(logging.INFO)
-    logging.getLogger("pogom.models").setLevel(logging.INFO)
-    logging.getLogger('werkzeug').setLevel(logging.ERROR)
-
-    config['parse_pokemon'] = not args.no_pokemon
-    config['parse_pokestops'] = not args.no_pokestops
-    config['parse_gyms'] = not args.no_gyms
-
-    # Turn these back up if debugging
-    if args.debug:
-        logging.getLogger("requests").setLevel(logging.DEBUG)
-        logging.getLogger("pgoapi").setLevel(logging.DEBUG)
-        logging.getLogger("rpc_api").setLevel(logging.DEBUG)
-
-    db = init_database()
-    if args.clear_db:
-        if args.db_type == 'mysql':
-            drop_tables(db)
-        elif os.path.isfile(args.db):
-            os.remove(args.db)
-    create_tables(db)
-
-    position = get_pos_by_name(args.location)
-    if not any(position):
-        log.error('Could not get a position by name, aborting.')
-        sys.exit()
-
-    log.info('Parsed location is: {:.4f}/{:.4f}/{:.4f} (lat/lng/alt)'.
-             format(*position))
-    if args.no_pokemon:
-        log.info('Parsing of Pokemon disabled.')
-    if args.no_pokestops:
-        log.info('Parsing of Pokestops disabled.')
-    if args.no_gyms:
-        log.info('Parsing of Gyms disabled.')
-
-    config['ORIGINAL_LATITUDE'] = position[0]
-    config['ORIGINAL_LONGITUDE'] = position[1]
-    config['LOCALE'] = args.locale
-    config['CHINA'] = args.china
-    config['ROOT_PATH'] = app.root_path
-    config['GMAPS_KEY'] = args.gmaps_key
-    config['REQ_SLEEP'] = args.scan_delay
-
-    create_search_threads(args.num_threads)
