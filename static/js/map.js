380396a148cfed7b480f3e394e069bf2139fa288
deirh <deirh@seznam.cz>
2016-08-02 21:51:50 +0200

    Pulse size back to 70

--- static/js/map.js
+++ static/js/map.js
@@ -695,7 +695,7 @@ function setupScannedMarker(item) {
     map: map,
     clickable: false,
     center: circleCenter,
-    radius: 42, // metres
+    radius: 60, // metres
     fillColor: getColorByDate(item.last_modified),
     strokeWeight: 1,
     clickable: false,
