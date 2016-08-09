b825b0d4a9ec0efc159e2aa382a5d25820a83f74
deirh <deirh@seznam.cz>
2016-08-09 03:44:00 +0200

    Revert "Larger commits"
    This reverts commit a6bfa1fe079cfd0bf1603c2676cc4273f3b6c8d3.

--- start_scanner.bat
+++
@@ -1,1 +0,0 @@
-python runserver.py -a ptc -u abelasim -p elokut7 -st 15 -l "50.051535,14.439922" -P 16677 -k "AIzaSyCaAo-UcoeeWRAhb9xSQPLfW0UdOR1PF8A"
--- start_web.bat
+++
@@ -1,1 +0,0 @@
-python runserver.py -a google -u mojeprababicka -p dedececk -st 15 -l "50.051535,14.439922" -os -H 127.0.0.1 -P 16677 -t 25 -k "AIzaSyCaAo-UcoeeWRAhb9xSQPLfW0UdOR1PF8A"
