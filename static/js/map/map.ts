ca179b7dea3bdf2099c91705d2c8b85517fd0059
deirh <deirh@seznam.cz>
2016-08-29 01:50:31 +0200

    v2.0

--- static/js/map/map.ts
+++ static/js/map/map.ts
@@ -1,11 +1,8 @@
 /// <reference path="../../../typings/globals/require/index.d.ts" />
 
-define((require) => {
-    require(["async!https://maps.googleapis.com/maps/api/js?key="
-    + googleApiKey + "&callback=initMap&libraries=places,geometry"], () => {
-        let map = require("../map");
-        map.initMap();
-        return { map };
-    });
-})
+let googleMaps = require(["async!https://maps.googleapis.com/maps/api/js?key="
+    + googleApiKey + "&callback=initMap&libraries=places,geometry"]);
+let map = require("../map");
+map.initMap();
+export { map };
 
