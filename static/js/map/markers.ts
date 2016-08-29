885d46d16d73d19f0d8008aa10d13ad8f8c2d966
deirh <deirh@seznam.cz>
2016-08-29 22:54:26 +0200

    Syntax cosmetics

--- static/js/map/markers.ts
+++ static/js/map/markers.ts
@@ -1,26 +1,26 @@
 /// <reference path="../../../typings/globals/require/index.d.ts" />
 
-import {map, google} from "./core";
 import {Store} from "../store";
+import {google, map} from "./core";
 
 export function createSearchMarker (lat, lng) {
   let searchMarker = new google.maps.Marker({ // need to keep reference.
     position: {lat, lng },
-    map: map,
+    map,
     animation: google.maps.Animation.DROP,
-    draggable: !Store.get('lockMarker'),
+    draggable: !Store.get("lockMarker"),
     icon: null,
     optimized: false,
-    zIndex: google.maps.Marker.MAX_ZINDEX + 1
+    zIndex: google.maps.Marker.MAX_ZINDEX + 1,
   });
 
-  var oldLocation = null;
-  google.maps.event.addListener(searchMarker, 'dragstart', function () {
+  let oldLocation = null;
+  google.maps.event.addListener(searchMarker, "dragstart", () => {
     oldLocation = searchMarker.getPosition();
   });
 
-  google.maps.event.addListener(searchMarker, 'dragend', function () {
-    var newLocation = searchMarker.getPosition();
+  google.maps.event.addListener(searchMarker, "dragend", () => {
+    // let newLocation = searchMarker.getPosition();
   });
 
   return searchMarker;
