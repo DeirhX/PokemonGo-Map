dbd3fbb36e77f8267def27f86662b44e0e8134ca
deirh <deirh@seznam.cz>
2016-09-06 01:10:05 +0200

    Totally spawn analysis

--- static/js/interface/bar/spawnbar.ts
+++ static/js/interface/bar/spawnbar.ts
@@ -6,8 +6,8 @@ export class SpawnBar extends Bar {
     public displaySpawn(spawnDetail: ISpawnDetail) {
         let $root = $(this.getRoot()).find(".spawn-detail");
         $root.data("spawn", spawnDetail);
-        $root.find(".overall.spawn-table").html(overallProbabilityTable(spawnDetail));
-        $root.find(".hourly.spawn-table").html(hourlyProbabilityTable(spawnDetail));
+        $root.find(".overall.spawn-table").html(overallProbabilityTable(spawnDetail, 16));
+        $root.find(".hourly.spawn-table").html(hourlyProbabilityTable(spawnDetail, 7));
         updateSpawnTooltip(spawnDetail, this.getRoot(), true);
     }
 }
