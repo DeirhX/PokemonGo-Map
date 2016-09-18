ae50686d1ec95d6fee75f6fd5ba810ce175774d2
deirh <deirh@seznam.cz>
2016-09-18 13:06:34 +0200

    Removed autogenerated

--- static/js/interface/bar/statsbar.js
+++
@@ -1,7 +0,0 @@
-define(["require", "exports", "../bar"], function (require, exports, bar_1) {
-    "use strict";
-    var statsBar = new bar_1.Bar("stats");
-    exports.__esModule = true;
-    exports["default"] = statsBar;
-});
-//# sourceMappingURL=statsbar.js.map
