da7cd402cd2d2a5caa45c5f28c2e0bf3ba7cef7e
deirh <deirh@seznam.cz>
2016-09-19 02:09:57 +0200

    Nicer account details

--- static/js/members/visual.ts
+++ static/js/members/visual.ts
@@ -1,16 +1,20 @@
 /// <reference path="../../../typings/globals/jquery/index.d.ts" />
 
+import {IMember} from "./server";
+
 $("#logged-in").hide();
 $("#logged-out").hide();
 
-export function applyLoginState (userInfo) {
+export function applyLoginState (userInfo: IMember) {
     if (userInfo.id) {
         $("#logged-out").hide();
         $("#logged-in").show();
-        $("#email").html(userInfo.email);
+        $("#email").html(userInfo.email + "#" + userInfo.id);
+        $("#username").val(userInfo.username);
     } else {
         $("#logged-in").hide();
         $("#logged-out").show();
         $("#email").html();
+        $("#username").val();
     }
 }
