a8ce696cc1c5eea85cec57b1fcc76573f586faa2
deirh <deirh@seznam.cz>
2016-09-20 01:58:37 +0200

    Filling in shared owned locations

--- static/js/members/visual.ts
+++ static/js/members/visual.ts
@@ -5,18 +5,35 @@ import {IMember} from "./server";
 $("#logged-in").hide();
 $("#logged-out").hide();
 
-export function applyLoginState (userInfo: IMember) {
-    if (userInfo.id) {
+export function applyLoginState (member: IMember) {
+    if (member.id) {
         // console.log("Applying login state");
         $("#logged-out").hide();
         $("#logged-in").show();
-        $("#email").html(userInfo.email + "#" + userInfo.id);
-        $("#username").val(userInfo.username);
+        $("#email").html(member.email + "#" + member.id);
+        $("#username").val(member.username);
+        let sharedLocations = $("#shared-locations");
+        let ownedLocations = $("#owned-locations");
+        sharedLocations.html();
+        for (let location of member.locations) {
+            if (location.relation === 0) {
+                sharedLocations.append($("<option></option>").attr("value", location.id).text(location.name));
+            } else if (location.relation === 1) {
+                ownedLocations.append($("<option></option>").attr("value", location.id).text(location.name));
+            }
+        }
     } else {
         // console.log("Applying logged-out state");
         $("#logged-in").hide();
         $("#logged-out").show();
         $("#email").html();
         $("#username").val();
+        let sharedLocations = $("#shared-locations-guest");
+        sharedLocations.html();
+        for (let location of member.locations) {
+            if (location.relation === 0) {
+                sharedLocations.append($("<option></option>").attr("value", location.id).text(location.name));
+            }
+        }
     }
 }
