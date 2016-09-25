1cd921374db766b77af712f59db8f2f256d5750e
deirh <deirh@seznam.cz>
2016-09-25 15:07:35 +0200

    Notifications caused exceptions on android

--- static/js/notifications.ts
+++ static/js/notifications.ts
@@ -1,5 +1,7 @@
 import {core} from "./core/base";
 declare var Notification; // TODO: Get api typing
+let notificationsSupported = false;
+declare var ServiceWorkerRegistration;
 
 export let notifiedPokemon = [];
 export let notifiedRarity = [];
@@ -10,43 +12,83 @@ if (Audio) { // Damn Safari. Planet of the apes.
 }
 
 
-export function initNotifications() {
+export function initNotifications(): boolean {
+    /*
+    if (ServiceWorkerRegistration && ServiceWorkerRegistration.showNotification) {
+        console.log("New notification system activated.");
+        if (Notification.permission !== "granted") {
+            Notification.requestPermission();
+        }
+        return true;
+    }
+    */
+
     if (!("Notification" in window)) {
         return false; // Notifications are not present in browser
     }
     if (!Notification) {
         console.log("could not load notifications");
-        return;
+        return false;
     }
 
     if (Notification.permission !== "granted") {
         Notification.requestPermission();
+        notificationsSupported = true;
+        return true;
     }
 }
 
 export function sendNotification (title, text, icon, lat, lng) {
-    if (!("Notification" in window)) {
+    if (!notificationsSupported) {
         return false; // Notifications are not present in browser
     }
 
-    if (Notification.permission !== "granted") {
-        Notification.requestPermission();
-    } else {
-        const notification = new Notification(title, {
-            icon,
-            body: text,
-            sound: "sounds/ding.mp3",
-        });
+    let onClick = () => {
+        window.focus();
+        core.map.centerMap(lat, lng, 20);
+    };
 
-        notification.onclick = () => {
-            window.focus();
-            notification.close();
 
-            core.map.centerMap(lat, lng, 20);
-        };
+
+    {
+        if (Notification.permission !== "granted") {
+            Notification.requestPermission();
+        } else {
+            try {
+                const notification = new Notification(title, {
+                    icon,
+                    body: text,
+                    sound: "sounds/ding.mp3",
+                });
+                notification.onclick = () => {onClick(); notification.close(); };
+            } catch (e) {
+            if (e.name === "TypeError") {
+                notificationsSupported = false;
+                return false;
+            }
+        }
     }
 }
 
 export function playNotifySound() {
     notifySound.play();
+}
+
+
+function isNewNotificationSupported(): boolean {
+    if (!("Notification" in window) || !Notification.requestPermission) {
+        return false;
+    }
+    if (Notification.permission === "granted") {
+        throw new Error('You must only call this *before* calling Notification.requestPermission(), otherwise this feature detect would bug the user with an actual notification!');
+    }
+
+    try {
+        new Notification("");
+    } catch (e) {
+        if (e.name === 'TypeError') {
+            return false;
+        }
+    }
+    return true;
 }
