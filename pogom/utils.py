ff6befcc9dba3687d6cdf3b74964cd5baa1b8fa3
deirh <deirh@seznam.cz>
2016-10-05 01:40:19 +0200

    Encounters and gym info now defaults to False

--- pogom/utils.py
+++ pogom/utils.py
@@ -161,7 +161,7 @@ def get_args():
                         type=int, default=0)
     parser.add_argument('-enc', '--encounter',
                         help='Start an encounter to gather IVs and moves',
-                        action='store_true', default=True)
+                        action='store_true', default=False)
     parser.add_argument('-ed', '--encounter-delay',
                         help='Time delay between encounter pokemon in scan threads',
                         type=float, default=1)
@@ -170,8 +170,8 @@ def get_args():
                                 help='List of pokemon to encounter for more stats')
     encounter_list.add_argument('-eblk', '--encounter-blacklist', action='append', default=[],
                                 help='List of pokemon to NOT encounter for more stats')
-    parser.add_argument('-gi', '--gym-info', help='Get all details about gyms (causes an additional API hit for every gym)',
-                        action='store_true', default=True)
+    parser.add_argument('-gym', '--gym-info', help='Get all details about gyms (causes an additional API hit for every gym)',
+                        action='store_true', default=False)
     parser.add_argument('-px', '--proxy', help='Proxy url (e.g. socks5://127.0.0.1:9050)', action='append')
     parser.add_argument('-pxsc', '--proxy-skip-check', help='Disable checking of proxies before start', action='store_true', default=False)
     parser.add_argument('-pxt', '--proxy-timeout', help='Timeout settings for proxy checker in seconds ', type=int, default=5)
