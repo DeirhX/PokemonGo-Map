9bb8fd51f42b27cd0914d7feddc7b9d87b7eb53e
deirh <deirh@seznam.cz>
2016-10-06 22:15:15 +0200

    Logging fixored

--- extend/configure.py
+++ extend/configure.py
@@ -46,6 +46,7 @@ def configure(app):
 
     # These are very noisey, let's shush them up a bit
     logging.getLogger('peewee').setLevel(logging.INFO)
+    logging.getLogger('pogom.search').setLevel(logging.INFO)
     logging.getLogger('requests').setLevel(logging.WARNING)
     logging.getLogger('pgoapi.pgoapi').setLevel(logging.WARNING)
     logging.getLogger('pgoapi.rpc_api').setLevel(logging.INFO)
